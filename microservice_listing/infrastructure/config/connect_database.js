2b9dbc62a6dde2e3bfe0f3e56f4b5a14cd96c502
GilbertoSLeite <gleite@lsconsultorias.srv.br>
2021-10-01 16:30:41 -0300

    Tentando Configurar o Heroku

--- microservice_listing/infrastructure/config/connect_database.js
+++ microservice_listing/infrastructure/config/connect_database.js
@@ -3,7 +3,8 @@ const { Client } = require("pg");
 const dbConf = require("./db_config");
 
 const client = new Client({
-  connectionString: dbConf.HOST,
+  connectionString:
+    "postgres://gggoevhglzwrlp:28baf177da038ef25b6a94e7baa2a79b92bb156e9bb919fa9d62ee0379103728@ec2-52-22-161-59.compute-1.amazonaws.com:5432/d14tkqm3den25t",
   ssl: {
     rejectUnauthorized: false,
   },
--- microservice_listing/infrastructure/config/db_config.js
+++ microservice_listing/infrastructure/config/db_config.js
@@ -1,8 +1,8 @@
 module.exports = {
   HOST: "ec2-52-22-161-59.compute-1.amazonaws.com",
+  DB: "d14tkqm3den25t",
   USER: "gggoevhglzwrlp",
   PASSWORD: "28baf177da038ef25b6a94e7baa2a79b92bb156e9bb919fa9d62ee0379103728",
-  DB: "d14tkqm3den25t",
   dialect: "postgres",
   sslmode: "require",
   pool: {
