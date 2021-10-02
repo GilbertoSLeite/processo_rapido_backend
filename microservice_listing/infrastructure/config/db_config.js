6a5ef8189a47d60c27106a11b1f6df6f21c997aa
GilbertoSLeite <gleite@lsconsultorias.srv.br>
2021-10-01 23:05:14 -0300

    Dor de Cabeça

--- microservice_listing/infrastructure/config/db_config.js
+++ microservice_listing/infrastructure/config/db_config.js
@@ -3,6 +3,7 @@ module.exports = {
   DB: "d14tkqm3den25t",
   USER: "gggoevhglzwrlp",
   PASSWORD: "28baf177da038ef25b6a94e7baa2a79b92bb156e9bb919fa9d62ee0379103728",
+  ssl: true,
   dialect: "postgres",
   dialectOptions: {
     ssl: true,
