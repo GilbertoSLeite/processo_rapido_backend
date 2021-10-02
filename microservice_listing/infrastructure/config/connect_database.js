50e3b430878a06b6ed1eff1c32a60c36bc6b947f
GilbertoSLeite <gleite@lsconsultorias.srv.br>
2021-10-01 22:45:21 -0300

    Heroku Dando Trabalho

--- microservice_listing/infrastructure/config/connect_database.js
+++ microservice_listing/infrastructure/config/connect_database.js
@@ -15,7 +15,7 @@ module.exports = new Sequelize(dbConf.DB, dbConf.USER, dbConf.PASSWORD, {
   host: dbConf.HOST,
   dialect: dbConf.dialect,
   operatorsAliases: 0, // Ao invés de usar false ou true usar 0 ou 1
-  ssl: dbConf.ssl,
+  dialectOptions: dbConf.dialectOptions,
   pool: {
     max: dbConf.pool.max,
     min: dbConf.pool.min,
--- microservice_listing/infrastructure/config/db_config.js
+++ microservice_listing/infrastructure/config/db_config.js
@@ -4,7 +4,9 @@ module.exports = {
   USER: "gggoevhglzwrlp",
   PASSWORD: "28baf177da038ef25b6a94e7baa2a79b92bb156e9bb919fa9d62ee0379103728",
   dialect: "postgres",
-  ssl: true,
+  dialectOptions: {
+    ssl: true,
+  },
   pool: {
     max: 5,
     min: 0,
